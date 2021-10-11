dc1aa94df04349f0fff5f94a4e59e50f0a67299f
Ivan Karabadzhak <jakeroid@icloud.com>
2021-10-11 17:50:18 +0300

    Added support for input arguments. Added support for downscale height. Changed .gitignore file.

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
+import argparse
 from PIL import Image
 from resizeimage import resizeimage
 from os import listdir
 from os.path import isfile, join
 
-input_path = './input'
-output_path = './output'
 
-files = [f for f in listdir(input_path) if isfile(join(input_path, f)) and f != '.DS_Store']
+description = 'Script could downscale images to specific width/height.'
+usage = '%(prog)s type value input_dir output_dir'
+parser = argparse.ArgumentParser(description=description, usage=usage)
+parser.add_argument('type', help='could be width or height')
+parser.add_argument('value', help='pixels count')
+parser.add_argument('input_dir', help='input directory with images')
+parser.add_argument('output_dir', help='output directory with images')
+args = parser.parse_args()
+
+
+files = []
+for f in listdir(args.input_dir):
+
+    f_path = join(args.input_dir, f)
+    if not isfile(f_path):
+        continue
+
+    if f[0] == '.':
+        continue
+
+    files.append(f)
+
 
 for f in files:
+
     print('Processing ' + f + '...')
-    input_f = join(input_path, f)
-    output_f = join(output_path, f)
+
+    input_f = join(args.input_dir, f)
+    output_f = join(args.output_dir, f)
+
     fd_img = open(input_f, 'rb')
     img = Image.open(fd_img)
-    img = resizeimage.resize_width(img, 2250)
+
+    target_value = int(args.value)
+    if args.type == 'width':
+        img = resizeimage.resize_width(img, target_value)
+    elif args.type == 'height':
+        img = resizeimage.resize_height(img, target_value)
+
     img.save(output_f, img.format)
     fd_img.close()
+
 
 print('Finished!')
 
